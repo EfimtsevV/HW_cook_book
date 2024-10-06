969e79983ff143e79be4e94421d6a3b2f57b1895
efimtsevv <efimtsevva@mail.ru>
2024-10-06 19:44:31 +0300

    Add print

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ cook_book_1 = Cook_book()
  
 # Задание 1 - создание словаря с ингридиентами
 
-All_ingredients = (cook_book_1.parse_recipes('recipes.txt'))
+all_dishes = (cook_book_1.parse_recipes('recipes.txt'))
+
+
 
 # Задание 2 - создание словаря с ингридиентами и количеством гостей
 
-shop_list = cook_book_1.get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+n = 2 # Количество гостей
+shop_list = cook_book_1.get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], n)
 
+print(f'Все блюда: \n {all_dishes}\n Список ингридиентов на {n} человек(а): \n {shop_list}')
